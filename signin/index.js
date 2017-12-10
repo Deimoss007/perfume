a8a1e6454c84fc0962ce915928413d8297d2ae5d
Deimoss007 <fitchertops@gmail.com>
2017-12-10 16:34:07 -0500

    Delete index.js

--- signin/index.js
+++
@@ -1,8 +0,0 @@
-var page = require('page');
-var empty = require('empty-element');
-var template = require('./template');
-
-page('/signin', function (ctx, next) {
-    var main = document.getElementById('main-container');
-    empty(main).appendChild(template);
-})
